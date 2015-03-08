a9dab59bece6b47db6c7d9d2f7c1a4d7cfb544ea
Bartuz <filip.bartuzi@gmail.com>
2015-03-09 00:45:19 +0100

    Add rectangle repo

---
+++ lecture01/rectangle/src/Rectangle.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+/**
+ * Created by filip on 08/03/15….
+ * Copyright (c) 2015. Filip Bartuzi
+ */
+public class Rectangle {
+
+    private Point leftTop;
+    private Point bottomRight;
+}
